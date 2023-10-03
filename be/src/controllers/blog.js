a33ab90bb7a3c2c9c1bc1d292dc0c43320bf79de
KayCCCCCC <nguyenngothanhnha2003kg@gmail.com>
2023-10-03 20:45:35 +0700

    deleteBlog

--- be/src/controllers/blog.js
+++ be/src/controllers/blog.js
@@ -1,12 +1,30 @@
 const db = require("../models/index");
-const ServicePackage = db.servicePackage;
+const Blog = db.blog;
 
 class BlogController {
   static async createBlog(req, res) {
     // console.log(req.body);
     const thumbnail = req.file;
   }
-  static async getBlogById(req, res) {}
-  static async getAllBlog(req, res) {}
+  static async getBlogById(req, res) { }
+  static async getAllBlog(req, res) { }
+  static async deleteBlog(req, res) {
+    try {
+      const blogID = req.body.id;
+      const blog = await Blog.findByPk(blogID);
+      console.log(blog);
+      if (!blog) {
+        return res.status(404).json({ message: "Blog not found" });
+      }
+      await blog.destroy({
+        where: {
+          id: blogID,
+        },
+      });
+      return res.status(200).json({ message: "Remove blog successfully" });
+    } catch (error) {
+      return res.status(500).json({ message: "Can not remove!" });
+    }
+  }
 }
 exports.BlogController = BlogController;
--- be/src/routes/blog.js
+++ be/src/routes/blog.js
@@ -6,5 +6,6 @@ const upload = require("../utils/storageImage");
 router.post("/create", upload.single("thumbnail"), BlogController.createBlog);
 router.get("/get/:id", BlogController.getBlogById);
 router.get("/get-all", BlogController.getAllBlog);
+router.delete("/delete", BlogController.deleteBlog);
 
 module.exports = router;
