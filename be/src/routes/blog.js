179973f1ceace20a340828f8837882d813334a09
H1710 <huyquac@gmail.com>
2023-10-07 10:37:54 +0700

    test

--- be/src/routes/blog.js
+++ be/src/routes/blog.js
@@ -6,5 +6,7 @@ const upload = require("../utils/storageImage");
 router.post("/create", upload.single("thumbnail"), BlogController.createBlog);
 router.get("/get/:id", BlogController.getBlogById);
 router.get("/get-all", BlogController.getAllBlog);
-
+router.get("/get-all", BlogController.getAllBlog);
+router.get("/get-all", BlogController.getAllBlog);
+router.get("/get-all", BlogController.getAllBlog);
 module.exports = router;
