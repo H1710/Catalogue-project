ae55b94375d351151d6fc01b3d6d7c3ee1d68138
Dell@DESKTOP-LC8LQFT <luugiao.lm2003@gmail.com>
2023-10-12 22:34:01 +0700

    auto-create

--- be/src/routes/order.js
+++ be/src/routes/order.js
@@ -2,5 +2,6 @@ const { OrderController } = require("../controllers/order");
 const router = require("express").Router();
 
 router.post("/add-order", OrderController.addOrder);
+router.post("/auto-create-order", OrderController.autoCreateOrder);
 
 module.exports = router;
