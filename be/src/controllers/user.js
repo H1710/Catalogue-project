049bde5f429b2b09e116f47bdce370a44e50bc3b
hoanghuy2409 <tranhoanghuy489@gmail.com>
2023-10-22 22:01:32 +0700

    Update CRUD users

--- be/src/controllers/user.js
+++ be/src/controllers/user.js
@@ -19,6 +19,60 @@ class UserController {
       res.status(500).send({ message: "Something went wrong" });
     }
   }
+
+  static async getUserById(req, res) {
+    try {
+      const userId = req.params.id;
+      const user = await db.user.findByPk(userId);
+      if (user) {
+        res.status(200).send(user);
+      } else {
+        res.status(404).send({ message: "User not found" });
+      }
+    } catch (error) {
+      res.status(500).send({ message: "Something went wrong" });
+    }
+  }
+
+  static async updateUser(req, res) {
+    try {
+      const userId = req.params.id;
+      const newData = {
+        name: req.body.name,
+        address: req.body.address,
+        type_register: req.body.type_register,
+        email: req.body.email,
+        password: req.body.password,
+        end_date: req.body.end_date,
+      };
+
+      const user = await db.user.findByPk(userId);
+      if (user) {
+        await user.update(newData);
+        res.status(200).send(user);
+      } else {
+        res.status(404).send({ message: "User not found" });
+      }
+    } catch (error) {
+      res.status(500).send({ message: "Something went wrong" });
+    }
+  }
+
+  static async deleteUser(req, res) {
+    try {
+      const userId = req.params.id;
+      const user = await db.user.findByPk(userId);
+      if (user) {
+        await user.destroy();
+        res.status(204).send();
+      } else {
+        res.status(404).send({ message: "User not found" });
+      }
+    } catch (error) {
+      res.status(500).send({ message: "Something went wrong" });
+    }
+  }
+
 }
 
 exports.UserController = UserController;
--- be/src/routes/user.js
+++ be/src/routes/user.js
@@ -3,4 +3,7 @@ const { UserController } = require("../controllers/user");
 const router = require("express").Router();
 
 router.post("/create", UserController.createUser);
+router.get("/getUserById", UserController.getUserById);
+router.post("/updateUser", UserController.createUser);
+router.delete("/deleteUser", UserController.deleteUser);
 module.exports = router;
