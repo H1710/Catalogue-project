718e4727ee928f1241795b0c6075b53c0d48e568
KayCCCCCC <nguyenngothanhnha2003kg@gmail.com>
2023-11-16 03:42:55 +0700

    up

--- be/src/routes/template.js
+++ be/src/routes/template.js
@@ -4,9 +4,9 @@ const router = require("express").Router();
 const upload = require("../utils/storageImage");
 
 router.post(
-  "/create",
-  upload.single("thumbnail"),
-  TemplateController.createTemplate
+    "/create",
+    upload.single("thumbnail"),
+    TemplateController.createTemplate
 );
 router.get("/get/:templateId", TemplateController.getTemplate);
 router.get("/get-all", TemplateController.getAllTemplate);
