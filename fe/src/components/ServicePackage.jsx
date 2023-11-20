3aa510d18baac753fdd32c7fd79671d62136c8ab
hieutts <hieutt03.work@gmail.com>
2023-11-21 04:43:58 +0700

    servicepackefail

--- fe/src/components/ServicePackage.jsx
+++ fe/src/components/ServicePackage.jsx
@@ -5,6 +5,16 @@ import React, {
   useRef,
   useState,
 } from "react";
+import {
+  Alert,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 import { Dialog, Transition } from "@headlessui/react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
@@ -13,6 +23,9 @@ import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import CustomButton from "../components/common/Button";
 import { Divider, TextField, Typography } from "@mui/material";
 import InvoicesInfo from "./InvoicesInfo";
+import axios from "axios";
+import { useQueryClient } from "react-query";
+
 
 const lsPack = [
   {
@@ -63,11 +76,7 @@ export default function ServicePackage({
 
     setInfoInvoice({ ...infoInvoice, [e.target.name]: e.target.value });
   };
-  // console.log(infoInvoice);
-  // const handleCreateInvoice = () => {
-  //   closeModal();
-  //   setIsShowInvoice(true);
-  // };
+ 
 
   const handleNext = () => {
     setStateOfPopup((pre) => pre + 1);
@@ -80,9 +89,9 @@ export default function ServicePackage({
     initialValues: infoInvoice,
     onSubmit: (values) => {
       values.packageId = infoInvoice.packageId;
-      closeModal();
+      setIsShowInvoice(true);
+      // closeModal();
       setInfoInvoice(values);
-      setIsShowInvoice((prev) => !prev);
     },
 
     validationSchema: Yup.object({
@@ -101,6 +110,51 @@ export default function ServicePackage({
         .matches(/^(0[1-9]|1[0-2])\/\d{4}$/, "Must be in MM/YYYY format."),
     }),
   });
+  const [isPaid, setIsPaid] = useState(false);
+  const queryClient = useQueryClient();
+  const currentDate = Date.now();
+  const formattedDate = new Intl.DateTimeFormat("en-US", {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  }).format(currentDate);
+  const temp = [];
+  temp.push(
+    lsPack.find((item) => item.id.toString() === infoInvoice.packageId)
+  );
+  const expiryDate = new Date(
+    currentDate + temp[0].remainingDay * 24 * 60 * 60 * 1000
+  );
+  const formattedExpiryDate = new Intl.DateTimeFormat("en-US", {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  }).format(expiryDate);
+  const orderInfo = {
+    userId: user.id,
+    packageId: parseInt(infoInvoice.packageId),
+  };
+  const handlePay = useCallback(async () => {
+    try {
+      const rs = await axios
+        .post("http://localhost:5000/api/v1/order/add-order", orderInfo, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+        .then((res) => {
+          if (res.status !== 200) {
+            throw new Error("Error to paying order");
+          } else {
+            setIsPaid(true);
+            queryClient.invalidateQueries(["refresh_token"]);
+          }
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  }, [orderInfo]);
+
   return (
     <>
       <Transition appear show={showServiePackages} as={Fragment}>
@@ -128,7 +182,7 @@ export default function ServicePackage({
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-lg h-[450px]   transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+               {!isShowInvoice ? <Dialog.Panel className="w-1/2 h-[410px]   transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                   <Dialog.Title
                     as="h3"
                     className="text-xl font-normal leading-7 "
@@ -232,7 +286,7 @@ export default function ServicePackage({
                   {stateOfPopup === 2 && (
                     <form onSubmit={formik.handleSubmit} className="">
                       <div className="mt-2 mb-4 grid justify-items-stretch">
-                        <div className="text-lg pb-2 font-semibold justify-self-center">
+                        <div className="text-lg pb-2 font-semibold justify-self-start">
                           CARD INFORMATION
                         </div>
                         <div className="flex gap-2 items-center  mt-2">
@@ -292,12 +346,112 @@ export default function ServicePackage({
                     </form>
                   )}
                 </Dialog.Panel>
+                :
+                <Dialog.Panel className="w-1/2 min-h-[380px] transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Title
+                  as="h3"
+                  className="text-[25px] font-semibold leading-6 text-gray-900 flex items-center justify-center"
+                >
+                  ORDER INFORMATION {!isPaid && "PREVIEW"}
+                </Dialog.Title>
+                <div className="mt-2 flex flex-col grid justify-items-stretch">
+                  <div className="justify-self-end px-4  min-w-[200px]">
+                    Time: {formattedDate}
+                  </div>
+                  <div className="flex flex-row justify-between ">
+                    <div className="user mt-1 mb-2 min-w-[300px] ">
+                      <div className="font-semibold "> USER INFORMATION</div>
+                      <div>Username: {user.name}</div>
+                      <div>Email: {user.email}</div>
+                    </div>
+
+                    <div className="card mt-1 min-w-[200px] mr-2">
+                      <div className="font-semibold "> PAYING INFORMATION </div>
+                      <div>Card number: {infoInvoice.cardNumber}</div>
+                      <div>Security Code: {infoInvoice.security}</div>
+                      <div>Expiry day: {infoInvoice.expiry}</div>
+                    </div>
+                  </div>
+                  <div className="total mt-1 font-semibold  ">
+                    <TableContainer component={Paper}>
+                      <Table
+                        sx={{ minWidth: 700 }}
+                        size="medium"
+                        aria-label="a dense table"
+                      >
+                        <TableHead>
+                          <TableRow>
+                            <TableCell sx={{ fontWeight: "bold" }}>
+                              No.
+                            </TableCell>
+                            <TableCell align="left" sx={{ fontWeight: "bold" }}>
+                              Name
+                            </TableCell>
+                            <TableCell align="left" sx={{ fontWeight: "bold" }}>
+                              Description
+                            </TableCell>
+                            <TableCell align="left" sx={{ fontWeight: "bold" }}>
+                              Cost
+                            </TableCell>
+                          </TableRow>
+                        </TableHead>
+                        <TableBody>
+                          <TableRow>
+                            <TableCell>01</TableCell>
+                            <TableCell align="left">{temp[0].name}</TableCell>
+                            <TableCell align="left">
+                              You can use all productivity to{" "}
+                              {formattedExpiryDate}
+                            </TableCell>
+                            <TableCell align="left">{temp[0].cost}$</TableCell>
+                          </TableRow>
+                          <TableRow>
+                            <TableCell></TableCell>
+                            <TableCell></TableCell>
+                            <TableCell align="left" sx={{ fontWeight: "bold" }}>
+                              TOTAL
+                            </TableCell>
+                            <TableCell>{temp[0].cost}$</TableCell>
+                          </TableRow>
+                        </TableBody>
+                      </Table>
+                    </TableContainer>
+                  </div>
+                </div>
+
+                <div className="mt-4 flex justify-end gap-4">
+                  {isPaid && (
+                    <Alert severity="success">Paying successfully</Alert>
+                  )}
+                  {!isPaid && (
+                    <>
+                      <CustomButton
+                        classContent="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                        handleClick={handlePay}
+                        text="Pay"
+                      />
+                      <CustomButton
+                        text="Cancel"
+                        classContent="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      handleClick={closeModal}
+                      />
+                    </>
+                  )}
+                  {isPaid && (
+                    <CustomButton
+                      text="Close"
+                      classContent="inline-flex justify-center items-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      handleClick={closeModal}
+                    />
+                  )}
+                </div>
+              </Dialog.Panel>}
               </Transition.Child>
             </div>
           </div>
         </Dialog>
       </Transition>
-      {isShowInvoice && (
+      {/* {isShowInvoice && (
         <InvoicesInfo
           isShowInvoice={isShowInvoice}
           setIsShowInvoice={setIsShowInvoice}
@@ -305,7 +459,7 @@ export default function ServicePackage({
           infoInvoice={infoInvoice}
           lsPack={lsPack}
         />
-      )}
+      )} */}
     </>
   );
 }
